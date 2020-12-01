cad425e6ec578fe315e45d41f8a1b5d06ab2dbcb
BloodyValentine <69511939+YuriPontes1989@users.noreply.github.com>
2020-12-01 20:53:59 -0300

    Add files via upload

---
+++ Moedas/Moedas/MoedasClass.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moedas
+{
+    class MoedasClass
+    {
+
+        public static double moedas;
+        public static double valor;
+        public static double Ifin = 1.06;
+
+
+        public static double Reais()//Calcular em real
+        {
+            return moedas * (valor * Ifin);
+        }
+    }
+}
---
+++ Moedas/Moedas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Qual é o valor do dólar? "); // Usuário escreve 
+            MoedasClass.moedas = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares deseja comprar? ");
+            MoedasClass.valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor a ser pago em reais R$: " + MoedasClass.Reais().ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadKey();
+        }
+    }
+}
